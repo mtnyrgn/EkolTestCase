dfc0b85abdc7a5cc94e7f4c575e7cb38fd660c7e
mtnyrgn <metinyorgun1996@gmail.com>
2022-07-26 23:54:06 +0300

    added comment lines to solution class

--- EkolTestCase/Solution.cs
+++ EkolTestCase/Solution.cs
@@ -10,28 +10,29 @@ namespace EkolTestCase
             Stack characterBox = new Stack();
             char[] tempText = text.ToCharArray();
 
-            tempText[0] = text[0];
+            characterBox.Push(tempText[0]); //İlk karakter için stack'e yükleme yapıldı.
 
-            characterBox.Push(tempText[0]);
             for (int i = 1; i < text.Length; i++)
             {
                 if (tempText[i - 1] == tempText[i])
                 {
+                    //Eger bir önceki karakterimle suanki karakterim match olursa Stack icerisine pushlayacagim
                     characterBox.Push(tempText[i]);
                 }
                 else
                 {
                     if (characterBox.Count == n)
                     {
+                        //Eger match olmazsa ve stackteki eleman sayımla n parametrem esitse oldugum yerden n kadar geriye gidip stringin o kısmını * ile isaretliyorum.
                         for (int j = i-1; j > i - n -1; j--)
                         {
                             tempText[j] = '*';
                         }
-                        characterBox.Clear();
+                        characterBox.Clear(); //İsaretleme isi bittikten sonra stackimi bosaltiyorum.
                     }
                     else
                     {
-                        characterBox.Clear();
+                        characterBox.Clear(); // Eger karakter degisiminde stackteki eleman sayim n parametremle esit degilse isaretleme yapmadan stackimi bosaltiyorum.
                     }
                 }
             }
